a2bafbdd64c13b09c079d9f7d1411f40b44ac6b4
Boris <glazunov2142@icloud.com>
2024-08-18 10:27:39 +0300

    Update parallel.go

--- internal/spider/domain/service/page/consumer/parallel.go
+++ internal/spider/domain/service/page/consumer/parallel.go
@@ -2,11 +2,11 @@ package pageconsumer
 
 import (
 	"context"
-	pagerepositoryinterface "github.com/Borislavv/scrapper/internal/spider/domain/repository/interface"
-	pagecomparatorinterface "github.com/Borislavv/scrapper/internal/spider/domain/service/page/comparator/interface"
-	pageconsumerinterface "github.com/Borislavv/scrapper/internal/spider/domain/service/page/consumer/interface"
-	scannerdtointerface "github.com/Borislavv/scrapper/internal/spider/domain/service/page/scanner/dto/interface"
-	logger "github.com/Borislavv/scrapper/internal/spider/infrastructure/logger/interface"
+	pagerepositoryinterface "gitlab.xbet.lan/web-backend/php/spider/internal/spider/domain/repository/interface"
+	pagecomparatorinterface "gitlab.xbet.lan/web-backend/php/spider/internal/spider/domain/service/page/comparator/interface"
+	pageconsumerinterface "gitlab.xbet.lan/web-backend/php/spider/internal/spider/domain/service/page/consumer/interface"
+	scannerdtointerface "gitlab.xbet.lan/web-backend/php/spider/internal/spider/domain/service/page/scanner/dto/interface"
+	logger "gitlab.xbet.lan/web-backend/php/spider/internal/spider/infrastructure/logger/interface"
 	"sync"
 )
 
@@ -39,7 +39,7 @@ func (c *Parallel) Consume(ctx context.Context, resultCh <-chan scannerdtointerf
 		go func(dto scannerdtointerface.Result) {
 			defer wg.Done()
 
-			if dto.Error() != nil {
+			if dto.Error() != nil { // check the scanner error for current page
 				c.logger.ErrorMsg(ctx, pageconsumerinterface.ScanURLError.Error(), logger.Fields{
 					"url":       dto.URL(),
 					"err":       dto.Error(),
@@ -48,9 +48,11 @@ func (c *Parallel) Consume(ctx context.Context, resultCh <-chan scannerdtointerf
 				return
 			}
 
+			// extract current page if errors are not exist
 			cur := dto.Page()
 
-			prev, found, err := c.repository.FindByURL(ctx, cur.URL)
+			// search a previous version of page
+			prev, found, err := c.repository.FindOneLatestByURL(ctx, cur.URL)
 			if err != nil {
 				c.logger.ErrorMsg(ctx, pageconsumerinterface.FindPageError.Error(), logger.Fields{
 					"url":       dto.URL(),
@@ -59,6 +61,7 @@ func (c *Parallel) Consume(ctx context.Context, resultCh <-chan scannerdtointerf
 				})
 				return
 			} else if !found {
+				// page was not found by url, it's mean the page is new (save it at first time)
 				if err = c.repository.Save(ctx, cur); err != nil {
 					c.logger.ErrorMsg(ctx, pageconsumerinterface.SavePageError.Error(), logger.Fields{
 						"url":       dto.URL(),
@@ -74,18 +77,41 @@ func (c *Parallel) Consume(ctx context.Context, resultCh <-chan scannerdtointerf
 				return
 			}
 
+			// check the previous version as actual
 			if !c.comparator.IsEquals(cur, prev) {
+				// set up new version for current page
+				cur.UpVersion(prev)
+
+				// save blinking page
 				if err = c.repository.Save(ctx, cur); err != nil {
 					c.logger.ErrorMsg(ctx, pageconsumerinterface.SavePageError.Error(), logger.Fields{
 						"url":       dto.URL(),
 						"err":       err.Error(),
 						"userAgent": dto.UserAgent(),
+						"version":   cur.Version,
+					})
+				} else {
+					c.logger.InfoMsg(ctx, pageconsumerinterface.BlinkingPageSavedMsg, logger.Fields{
+						"url":       cur.URL,
+						"userAgent": dto.UserAgent(),
+						"version":   cur.Version,
 					})
 				}
 			} else {
+				// update the previous page (set up new page.UpdatedAt value)
+				if err = c.repository.Update(ctx, prev); err != nil {
+					c.logger.ErrorMsg(ctx, pageconsumerinterface.SavePageError.Error(), logger.Fields{
+						"url":       dto.URL(),
+						"err":       err.Error(),
+						"userAgent": dto.UserAgent(),
+						"version":   prev.Version,
+					})
+				}
+
 				c.logger.InfoMsg(ctx, pageconsumerinterface.PagesAreEqualMsg, logger.Fields{
 					"url":       cur.URL,
 					"userAgent": dto.UserAgent(),
+					"version":   cur.Version,
 				})
 			}
 		}(dto)
