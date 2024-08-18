1fb4e801f840fb2c443ab8e5877a5b9bae05a766
Boris <glazunov2142@icloud.com>
2024-08-18 10:27:46 +0300

    Update provider.go

--- internal/spider/domain/service/page/provider/interface/provider.go
+++ internal/spider/domain/service/page/provider/interface/provider.go
@@ -2,7 +2,7 @@ package pageproviderinterface
 
 import (
 	"context"
-	scannerdtointerface "github.com/Borislavv/scrapper/internal/spider/domain/service/page/scanner/dto/interface"
+	scannerdtointerface "gitlab.xbet.lan/web-backend/php/spider/internal/spider/domain/service/page/scanner/dto/interface"
 	"net/url"
 )
 
