1e124fa905eea55050e9bf9b6be1a25394600cd7
Boris <glazunov2142@icloud.com>
2024-08-18 10:27:06 +0300

    Update page.go

--- internal/shared/domain/entity/page.go
+++ internal/shared/domain/entity/page.go
@@ -1,28 +1,54 @@
 package entity
 
 import (
-	"github.com/Borislavv/scrapper/internal/shared/infrastructure/vo"
+	"gitlab.xbet.lan/web-backend/php/spider/internal/shared/infrastructure/vo"
+	"time"
 )
 
 type Page struct {
-	vo.ID          `bson:",inline"`
-	URL            string              `bson:"url"`
-	UserAgent      string              `bson:"userAgent"`
-	Title          string              `bson:"title"`
-	Description    string              `bson:"description"`
-	Canonical      string              `bson:"canonical"`
-	H1             string              `bson:"h1"`
-	PlainText      string              `bson:"plainText"`
-	HTML           string              `bson:"html"`
-	FAQ            []string            `bson:"faq"`
-	RelinkingBlock []string            `bson:"relinkingBlock"`
-	HrefLangs      []string            `bson:"hrefLangs"`
-	Headers        map[string][]string `bson:"headers"`
+	vo.ID       `bson:",inline"`
+	Version     int    `bson:"version"`
+	URL         string `bson:"url"`
+	UserAgent   string `bson:"userAgent"`
+	Title       string `bson:"title"`
+	Description string `bson:"description"`
+	Canonical   string `bson:"canonical"`
+	H1          string `bson:"h1"`
+	PlainText   string `bson:"plainText"`
+	HTML        string `bson:"html"`
+	// FAQ is a map which contains map[question] = answer
+	FAQ map[string]string `bson:"faq"`
+	// HrefLangs is a map which contains map[rel + "_" + language] = link
+	HrefLangs map[string]string `bson:"hrefLangs"`
+	// Headers is a map of slices which contains map[headerName][][headerValue1, headerValue2, ...]
+	Headers map[string][]string `bson:"headers"`
+	// RelinkingBlock is a map of maps which contains map[blockTitle][anchor] = link
+	RelinkingBlock map[string]map[string]string `bson:"relinkingBlock"`
 	vo.Timestamp   `bson:",inline"`
+}
+
+func NewPage(url, userAgent string) *Page {
+	return &Page{
+		URL:       url,
+		UserAgent: userAgent,
+		Version:   1,
+		Timestamp: vo.Timestamp{
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+	}
 }
 
 func (p *Page) GetID() vo.ID {
 	return p.ID
+}
+
+func (p *Page) GetVersion() int {
+	return p.Version
+}
+
+func (p *Page) UpVersion(previous *Page) {
+	p.Version = previous.Version + 1
 }
 
 func (p *Page) GetURL() string {
@@ -49,15 +75,15 @@ func (p *Page) GetPlainText() string {
 	return p.PlainText
 }
 
-func (p *Page) GetFAQ() []string {
+func (p *Page) GetFAQ() map[string]string {
 	return p.FAQ
 }
 
-func (p *Page) GetRelinkingBlock() []string {
+func (p *Page) GetRelinkingBlock() map[string]map[string]string {
 	return p.RelinkingBlock
 }
 
-func (p *Page) GetHrefLangs() []string {
+func (p *Page) GetHrefLangs() map[string]string {
 	return p.HrefLangs
 }
 
