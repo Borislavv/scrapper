9cacc6111688e0dfb89797b2170ef0c160dbd61b
Boris <glazunov2142@icloud.com>
2024-08-18 10:27:27 +0300

    Update page.go

--- internal/spider/domain/repository/interface/page.go
+++ internal/spider/domain/repository/interface/page.go
@@ -3,14 +3,20 @@ package pagerepositoryinterface
 import (
 	"context"
 	"errors"
-	"github.com/Borislavv/scrapper/internal/shared/domain/entity"
+	"gitlab.xbet.lan/web-backend/php/spider/internal/shared/domain/entity"
 )
 
 var (
-	NotFoundError = errors.New("page not found")
+	FindByURLError       = errors.New("searching page by URL failed due to error occurred while executing query")
+	UpdateError          = errors.New("updating page failed due to error occurred while executing query")
+	UpdateMarshalError   = errors.New("updating page failed due to error occurred while marshaling page into []byte")
+	UpdateUnmarshalError = errors.New("updating page failed due to error occurred while unmarshaling []byte into bson.M")
+	SaveError            = errors.New("saving page failed due to error occurred while executing query")
+	InsertedIDCastError  = errors.New("casting inserted id failed to primitive.ObjectID")
 )
 
 type PageRepository interface {
-	FindByURL(ctx context.Context, url string) (page *entity.Page, found bool, err error)
+	FindOneLatestByURL(ctx context.Context, url string) (page *entity.Page, found bool, err error)
+	Update(ctx context.Context, page *entity.Page) error
 	Save(ctx context.Context, page *entity.Page) error
 }
