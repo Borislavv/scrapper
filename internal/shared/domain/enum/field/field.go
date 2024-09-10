3a3ad43fc29932052d7aa0440ba185ab3c8c9d9d
Boris <glazunov2142@icloud.com>
2024-09-10 10:50:35 +0300

    Create field.go

---
+++ internal/shared/domain/enum/field/field.go
@@ -0,0 +1,17 @@
+package fieldenum
+
+const (
+	Title           = "title"
+	Description     = "description"
+	Canonical       = "canonical"
+	H1              = "h1"
+	PlainText       = "plainText"
+	FAQ             = "faq"
+	HrefLangs       = "hrefLangs"
+	AlternateMedias = "alternateMedias"
+	RelinkingBlock  = "relinkingBlock"
+
+	FAQAnswer          = "answer"
+	HrefLangLink       = "href"
+	AlternateMediaLink = "href"
+)
