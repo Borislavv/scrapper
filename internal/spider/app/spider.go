9090a6e26682ed8bf27228434f9ba1f9841c96ee
Boris <glazunov2142@icloud.com>
2024-09-10 10:52:09 +0300

    Update spider.go

--- internal/spider/app/spider.go
+++ internal/spider/app/spider.go
@@ -2,8 +2,9 @@ package spider
 
 import (
 	"context"
-	sharedconfig "github.com/Borislavv/scrapper/internal/shared/app/config"
+	loggerinterface "github.com/Borislavv/scrapper/internal/shared/domain/service/logger/interface"
 	"github.com/Borislavv/scrapper/internal/shared/infrastructure/database"
+	liveness "github.com/Borislavv/scrapper/internal/shared/infrastructure/liveness/interface"
 	"github.com/Borislavv/scrapper/internal/shared/infrastructure/logger"
 	"github.com/Borislavv/scrapper/internal/spider/app/config"
 	spiderinterface "github.com/Borislavv/scrapper/internal/spider/app/config/interface"
@@ -16,15 +17,15 @@ import (
 	taskconsumer "github.com/Borislavv/scrapper/internal/spider/domain/service/task/consumer"
 	taskprovider "github.com/Borislavv/scrapper/internal/spider/domain/service/task/provider"
 	taskrunner "github.com/Borislavv/scrapper/internal/spider/domain/service/task/runner"
-	loggerinterface "github.com/Borislavv/scrapper/internal/spider/infrastructure/logger/interface"
 	pagerepository "github.com/Borislavv/scrapper/internal/spider/infrastructure/repository/page"
 	jobscheduler "github.com/Borislavv/scrapper/internal/spider/infrastructure/service/job/scheduler"
 	pageparser "github.com/Borislavv/scrapper/internal/spider/infrastructure/service/page/parser"
 	pagescanner "github.com/Borislavv/scrapper/internal/spider/infrastructure/service/page/scanner"
 	taskparser "github.com/Borislavv/scrapper/internal/spider/infrastructure/service/task/parser"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Spider struct {
+type App struct {
 	ctx          context.Context
 	cancel       context.CancelFunc
 	config       spiderinterface.Configurator
@@ -32,76 +33,88 @@ type Spider struct {
 	loggerCancel loggerinterface.CancelFunc
 	jobRunner    runnerinterface.JobRunner
 	jobScheduler schedulerinterface.JobScheduler
+	liveness     liveness.Prober
+	mongo        *mongo.Database
 }
 
-func New(ctx context.Context) (*Spider, error) {
+func New(ctx context.Context, output loggerinterface.Outputer, liveness liveness.Prober) (*App, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	_ = cancel
 
-	sharedCfg, err := sharedconfig.Load()
+	cfg, err := spiderconfig.Load()
 	if err != nil {
 		return nil, err
 	}
 
-	logrus, logrusCancel, err := logger.NewLogrus(sharedCfg)
+	logrus, logrusCancel, err := logger.NewLogrus(cfg, output)
 	if err != nil {
 		return nil, err
-	}
-
-	spiderCfg, err := spiderconfig.Load()
-	if err != nil {
-		return nil, logrus.Fatal(ctx, err, nil)
 	}
 
 	// infrastructure
-	mongodb, err := database.NewMongo(logrus).Connect(ctx, sharedCfg)
+	mongodb, err := database.NewMongo(logrus).Connect(ctx, cfg)
 	if err != nil {
 		return nil, logrus.Fatal(ctx, err, nil)
 	}
 
 	// page infra. dependencies
-	pageRepo := pagerepository.NewMongo(spiderCfg, logrus, mongodb)
+	pageRepo := pagerepository.NewMongo(cfg, logrus, mongodb)
 	pageParser := pageparser.NewHTML(logrus)
-	pageScanner := pagescanner.NewHTTP(spiderCfg, pageParser, logrus)
+	pageScanner := pagescanner.NewHTTP(cfg, pageParser, logrus)
 
 	// page domain dependencies
 	pageComparator := pagecomparator.NewEqual()
 	pageConsumer := pageconsumer.NewParallel(logrus, pageRepo, pageComparator)
-	pageProvider := pageprovider.NewChan(spiderCfg, pageScanner)
+	pageProvider := pageprovider.NewChan(cfg, pageScanner)
 
 	// task dependencies
-	taskParser := taskparser.NewCSV(spiderCfg, logrus)
-	taskProvider, err := taskprovider.NewParallel(ctx, logrus, spiderCfg, taskParser)
+	taskParser := taskparser.NewCSV(cfg, logrus)
+	taskProvider, err := taskprovider.NewParallel(ctx, logrus, cfg, taskParser)
 	if err != nil {
 		return nil, logrus.Fatal(ctx, err, nil)
 	}
-	taskRunner := taskrunner.New(spiderCfg, pageProvider, pageConsumer)
-	taskConsumer := taskconsumer.NewParallel(logrus, spiderCfg, taskRunner)
+	taskRunner := taskrunner.New(cfg, pageProvider, pageConsumer)
+	taskConsumer := taskconsumer.NewParallel(logrus, cfg, taskRunner)
 
 	// job dependencies
-	jobRunner := jobrunner.New(logrus, spiderCfg, taskConsumer, taskProvider)
-	jobScheduler := jobscheduler.NewTicker(spiderCfg)
+	jobRunner := jobrunner.New(logrus, cfg, taskConsumer, taskProvider)
+	jobScheduler := jobscheduler.NewTicker(cfg)
 
-	return &Spider{
+	return &App{
 		ctx:          ctx,
 		cancel:       cancel,
-		config:       spiderCfg,
+		config:       cfg,
 		logger:       logrus,
 		loggerCancel: logrusCancel,
 		jobRunner:    jobRunner,
 		jobScheduler: jobScheduler,
+		liveness:     liveness,
+		mongo:        mongodb,
 	}, nil
 }
 
-func (s *Spider) Start() {
-	defer s.Stop()
+func (a *App) Start() {
+	defer a.Stop()
+	a.logger.InfoMsg(a.ctx, "starting the spider application", nil)
 
-	for range s.jobScheduler.Manage(s.ctx) {
-		s.jobRunner.Run(s.ctx)
+	cancelLiveness := a.liveness.Watch(a)
+	defer cancelLiveness()
+
+	for range a.jobScheduler.Manage(a.ctx) {
+		a.jobRunner.Run(a.ctx)
 	}
 }
 
-func (s *Spider) Stop() {
-	s.cancel()
-	s.loggerCancel()
+func (a *App) Stop() {
+	a.logger.InfoMsg(a.ctx, "closing the spider application", nil)
+
+	a.cancel()
+	a.loggerCancel()
+}
+
+func (a *App) IsAlive() bool {
+	if err := a.mongo.Client().Ping(a.ctx, nil); err != nil {
+		return false
+	}
+	return true
 }
